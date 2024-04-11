8277becec3a99cbc44dcf012c5eb92667c4e4f89
leinformat <leinformat@gmail.com>
2024-04-11 15:24:34 -0500

    Add bg

--- siteData.php
+++ siteData.php
@@ -2,5 +2,5 @@
 $data = array(
     "title" => "Corporacion Cultural Cimarron Y Soga",
     "nit" => "900352675-6",
-    "metadescription"=> "La empresa Corporacion Cultural Cimarron Y Soga se encuentra situada en el departamento de META, en la localidad PUERTO GAITAN y su dirección postal es CALLE 8 8 06 BARRIO CENTRO PUERTO GAITAN META, PUERTO GAITAN, META.Corporacion Cultural Cimarron Y Soga está constituida como una ENTIDAD SIN ANIMO DE LUCRO. Si desea conocer más información sobre esta empresa, regístrese en eInforma y obtenga de forma gratuita su Informe Ampliado de Corporacion Cultural Cimarron Y Soga.La actividad a la que se dedica la empresa Corporacion Cultural Cimarron Y Soga es Actividades de otras asociaciones n c p.La última actualización de los datos de la empresa Corporacion Cultural Cimarron Y Soga ha sido el 11 de Abril de 2024."
+    "metadescription"=> "La empresa Corporacion Cultural Cimarron Y Soga se encuentra situada en el departamento de META, en la localidad PUERTO GAITAN y su dirección postal es CALLE 8 8 06 BARRIO CENTRO PUERTO GAITAN META, PUERTO GAITAN, META.Corporacion Cultural Cimarron Y Soga está constituida como una ENTIDAD SIN ANIMO DE LUCRO."
 );
