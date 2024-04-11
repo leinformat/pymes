a4031a8edca08dbf8266010836a8720f1affeb4f
leinformat <leinformat@gmail.com>
2024-04-11 14:07:11 -0500

    rename

--- siteData.php
+++ siteData.php
@@ -1,6 +1,6 @@
 <?php
 $data = array(
-    "title" => "Asocampos, Asociacion Campesina De Planas",
+    "title" => "CORPORACION CULTURAL CIMARRON Y SOGA",
     "nit" => "900533421-1",
     "metadescription"=> "somos una asociación sin animo de lucro, con el fin de promover proyectos del gremio de la agricultura, ganadería, forestal y todo lo relacionado por el campo el cual es con fines de proporcionar a los afiliados la participación en ellos."
 );
