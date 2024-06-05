a4c9d26303087a11a44d3ed75f1406fe259a5433
leinformat <leinformat@gmail.com>
2024-06-04 21:30:49 -0500

    New data

--- siteData.php
+++ siteData.php
@@ -1,6 +1,6 @@
 <?php
 $data = array(
     "title" => "Asociación Mano De Obra Local Perla Negra",
-    "nit" => "900798014-2",
+    "nit" => "901620320-0",
     "metadescription"=> "La empresa Asociacion De Trabajadores Perla Negra tiene como domicilio principal de su actividad la dirección, VEREDA PUERTO TRIUNFO en la ciudad de PUERTO GAITAN, META. El teléfono de Asociacion De Trabajadores Perla Negra es el 3134432757. Esta empresa fué constituida como ENTIDAD SIN ANIMO DE LUCRO y se dedica a Actividades de otras asociaciones n c p."
 );
