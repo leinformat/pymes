9934d8caae59631527014bbf3bfe51b9d74a2d47
leinformat <leinformat@gmail.com>
2024-11-09 11:54:49 -0500

    add bg

--- siteData.php
+++ siteData.php
@@ -1,6 +1,6 @@
 <?php
 $data = array(
-    "title" => "Asociacion de Ganaderos y Agricultores del Rincon",
-    "nit" => "901769531-9",
-    "metadescription"=> "somos una asociación sin animo de lucro, con el fin de promover proyectos del gremio de la agricultura, ganadería, forestal y todo lo relacionado por el campo el cual es con fines de proporcionar a los afiliados la participación en ellos."
+    "title" => "ASOCIACION DE PRODUCTORES AGROPECUARIOS DEL RIO ITEVIARE DEL DEPARTAMENTO DEL META",
+    "nit" => "900889539-8",
+    "metadescription"=> "Encuentra información de investigación de la empresa, datos de competidores, detalles de contacto y datos financieros de la ASOCIACIÓN DE PRODUCTORES AGROPECUARIOS DEL RÍO ITEVIARE DEL DEPARTAMENTO DEL META en MAPIRIPÁN, Meta"
 );
