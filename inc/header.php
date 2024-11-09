93b849e74fb79623a128e26168fd0826c7514672
leinformat <leinformat@gmail.com>
2024-11-09 12:14:02 -0500

    add logo

--- inc/header.php
+++ inc/header.php
@@ -5,13 +5,13 @@
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title><?php echo $data["title"] ?></title>
-	<meta name="description" content="<?php echo $data["metadescription"] ?>" />
+	<title><?= $data["title"] ?></title>
+	<meta name="description" content="<?= $data["metadescription"] ?>" />
 	<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
 	<link rel="stylesheet" href="css/bootstrap.min.css">
 	<link rel="stylesheet" href="css/style.css">
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-	<link rel="shortcut icon" href="logo.ico">
+	<link rel="shortcut icon" href="<?= $data["favicon"] ?>">
 	<link href="./fileinput/css/fileinput.min.css" media="all" rel="stylesheet" type="text/css" />
 
 </head>
@@ -22,10 +22,10 @@
 		<header class="pymes-repository__header-container">
 			<div class="pymes-repository__header">
 				<a class="pymes-repository__logo-link" href="index.php">
-				<img  class="pymes-repository__logo" src="logo.jpg" alt="<?php echo $data["title"] ?>">
+				<img  class="pymes-repository__logo" src="<?= $data["logo"] ?>" alt="<?= $data["title"] ?>">
 				</a>
 				<div class="pymes-repository__title-container">
-					<h1 class="pymes-repository__title"><?php echo $data["title"] ?></h1>
+					<h1 class="pymes-repository__title"><?= $data["title"] ?></h1>
 				</div>
 			</div>
 		</header>
--- siteData.php
+++ siteData.php
@@ -2,5 +2,8 @@
 $data = array(
     "title" => "ASOCIACION DE PRODUCTORES AGROPECUARIOS DEL RIO ITEVIARE DEL DEPARTAMENTO DEL META",
     "nit" => "900889539-8",
-    "metadescription"=> "Encuentra información de investigación de la empresa, datos de competidores, detalles de contacto y datos financieros de la ASOCIACIÓN DE PRODUCTORES AGROPECUARIOS DEL RÍO ITEVIARE DEL DEPARTAMENTO DEL META en MAPIRIPÁN, Meta"
+    "bg" => "./img/bg.jpg",
+    "logo" => "./img/logo.png",
+    "favicon" => "./img/logo.ico",
+    "metadescription" => "Encuentra información de investigación de la empresa, datos de competidores, detalles de contacto y datos financieros de la ASOCIACIÓN DE PRODUCTORES AGROPECUARIOS DEL RÍO ITEVIARE DEL DEPARTAMENTO DEL META en MAPIRIPÁN, Meta"
 );
